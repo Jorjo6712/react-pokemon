54e25087fcc7aabcd8860bf74804c58cc771009e
JorjoWasTaken <kiro.nicolov@gmail.com>
2024-05-06 22:30:07 +0200

    react uge 2

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import Home from './sections/Home'
+
+function App() {
+
+  return (
+    <>
+      <ChakraProvider>
+        <Home/>
+      </ChakraProvider>
+    </>
+  )
+}
+
+export default App
---
+++ src/sections/Home.tsx
@@ -0,0 +1,13 @@
+interface HomeProps {
+
+}
+
+const Home: React.FC<HomeProps> = () => {
+    return (
+        <>
+            <h1> hi </h1>
+        </>
+    )
+}
+
+export default Home
